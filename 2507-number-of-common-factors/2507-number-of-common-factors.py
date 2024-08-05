8f46fe7fb3ce0fa84ba4f5044c3f1e5a777cb78a
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-05 08:52:23 +0300

    Time: 34 ms (70.39%), Space: 16.5 MB (80.48%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.py
+++ 2507-number-of-common-factors/2507-number-of-common-factors.py
@@ -2,14 +2,12 @@ import math
 
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        gcd_ab = math.gcd(a, b)
         count = 0
-        sqrt_gcd = int(math.sqrt(gcd_ab))
-        
-        for i in range(1, sqrt_gcd + 1):
+        gcd_ab = math.gcd(a, b)
+        for i in range(1, int(math.sqrt(gcd_ab)) + 1):
             if gcd_ab % i == 0:
-                count += 1  
+                count += 1
                 if i != gcd_ab // i:
-                    count += 1  
+                    count += 1
         
         return count
