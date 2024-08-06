aad597ecb9d83b7f0892e0d68e31e9daf5020a74
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-06 08:54:48 +0300

    Time: 30 ms (83.84%), Space: 16.4 MB (89.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,10 +3,8 @@ class Solution:
         if n <= 2:
             return n
         
-       
         first = 1
         second = 2
-        
         
         for i in range(3, n + 1):
             current = first + second
