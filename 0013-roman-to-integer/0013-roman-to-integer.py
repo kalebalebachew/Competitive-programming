e5c1011c9abf1e1f8c747fcc4569212b5522c6d7
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-07 08:31:32 +0300

    Time: 53 ms (23.13%), Space: 16.5 MB (39.7%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,17 +1,26 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_to_int = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
         
+        special_cases = {
+            'IV': 4, 'IX': 9, 'XL': 40,
+            'XC': 90, 'CD': 400, 'CM': 900
+        }
+        
+        i = 0
         total = 0
-        prev_value = 0
+        length = len(s)
         
-        for char in s:
-            current_value = roman_to_int[char]
-            if current_value > prev_value:
-                total += current_value - 2 * prev_value
+        while i < length:
+            if i + 1 < length and s[i:i+2] in special_cases:
+                total += special_cases[s[i:i+2]]
+                i += 2
             else:
-                total += current_value
-            prev_value = current_value
+                total += roman_to_int[s[i]]
+                i += 1
         
         return total
 
