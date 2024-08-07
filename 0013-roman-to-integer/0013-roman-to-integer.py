1d173f910ae28c2c07aca85daf1822bec6d363c1
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-07 08:32:52 +0300

    Time: 48 ms (49.53%), Space: 16.6 MB (9.25%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,28 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_int = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50,
-            'C': 100, 'D': 500, 'M': 1000
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
-        
-        special_cases = {
-            'IV': 4, 'IX': 9, 'XL': 40,
-            'XC': 90, 'CD': 400, 'CM': 900
-        }
-        
-        i = 0
-        total = 0
-        length = len(s)
-        
-        while i < length:
-            if i + 1 < length and s[i:i+2] in special_cases:
-                total += special_cases[s[i:i+2]]
-                i += 2
-            else:
-                total += roman_to_int[s[i]]
-                i += 1
-        
-        return total
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
 
 
 
