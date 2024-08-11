dfc0cba47923b1ed52b505bfdf2527f9aeea8544
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-11 19:51:39 +0300

    Time: 100 ms (94.62%), Space: 19.1 MB (51.47%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,4 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        return reduce(lambda x, y: x ^ y, nums)
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
         
