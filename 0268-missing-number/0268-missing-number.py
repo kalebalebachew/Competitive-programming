8c191dd90d0503040943c929faf90ba988bd0206
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-12 09:43:54 +0300

    Time: 119 ms (30.02%), Space: 18.1 MB (12.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        d = {}
-        res = 0
-
-        for i, num in enumerate(nums):
-            d[num] = 1
-
+        num_map = {}
+    
+        for num in nums:
+            num_map[num] = True
+        
         for i in range(len(nums) + 1):
-            if i not in d:
-                res = i
-
-        return res
-            
+            if i not in num_map:
+                return i
         
