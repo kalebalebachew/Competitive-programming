0824789c95ad71c4e0d6f84698f7257dff337409
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-16 14:13:14 +0300

    Time: 30 ms (87.39%), Space: 16.6 MB (24.55%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,15 +1,14 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        d = {}
-        for s in sentence:
-            if s not in d:
-                d[s] = 1
-            else:
-                d[s] +=1
-        for key, value in d.items():
-            if key in d and len(d) == 26:
-                return True
+        s = sentence.lower()
+        k = set('abcdefghijklmnopqrstuvwxyz')
+    
+        if k.issubset(s):
+            return True
+        else:
             return False
+
+
             
             
         
