7a90fdf60e8d6b3f15bff5064ec74c06aceed20e
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2024-08-19 20:56:27 +0300

    Time: 100 ms (11.69%), Space: 21.1 MB (81.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [x for x, _ in Counter(nums).most_common(k)]
-            
-        
-                
-
+        freqDict = {}
+        res = []
+        for num in nums:
+            if num in freqDict:
+                freqDict[num] += 1
+            else:
+                freqDict[num] = 1
+        ans = sorted(freqDict.values(), reverse = True)
+        ans = [ans[x] for x in range(k)]
+        for key, value in freqDict.items():
+            if value in ans:
+                res.append(key)
+        return res
