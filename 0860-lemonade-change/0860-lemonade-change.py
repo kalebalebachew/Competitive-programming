291d9778f30b376998ad34c4acb1b957d1aa1192
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-01-14 22:05:50 +0300

    Time: 3 ms (78.34%), Space: 22 MB (5.5%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        dic = {5: 0, 10: 0}  
+        for bill in bills:
+            if bill == 5:
+                dic[5] += 1  
+            elif bill == 10:
+                if dic[5] == 0:  
+                    return False
+                dic[5] -= 1 
+                dic[10] += 1  
+            elif bill == 20:
+                if dic[10] > 0 and dic[5] > 0:
+                    dic[10] -= 1
+                    dic[5] -= 1
+                elif dic[5] >= 3:  
+                    dic[5] -= 3
+                else:
+                    return False  
+        return True
