58639c149e81c50c1aa0be8cf47ee35a6cd6c54c
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-02-17 20:33:10 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/A - Nathan is rich 269491.py
@@ -0,0 +1,15 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    wheels = int(input())
+    count = 0
+    count += wheels // 4
+    wheels = wheels % 4
+    count += wheels // 2
+    
+    print(count)
+        
+    
+    
