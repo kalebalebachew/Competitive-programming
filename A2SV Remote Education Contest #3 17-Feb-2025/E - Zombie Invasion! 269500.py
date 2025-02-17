b1fba1b02d207fd30b673f0b947efc73b1dabb28
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-02-17 20:44:25 +0300

    Created 'E - Zombie Invasion!' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/E - Zombie Invasion! 269500.py
@@ -0,0 +1,25 @@
+# Problem: E - Zombie Invasion! - https://codeforces.com/gym/588094/problem/E
+
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    hp = list(map(int, input().split()))
+    pos = list(map(int, input().split()))
+    
+    zombies = []
+    for i in range(n):
+        d = abs(pos[i])
+        zombies.append((d, hp[i]))
+    
+    zombies.sort(key=lambda z: z[0])
+    
+    total = 0  
+    possible = True
+    for d, h in zombies:
+        total += h
+        if total > d * k:
+            possible = False
+            break
+    
+    print("YES" if possible else "NO")
