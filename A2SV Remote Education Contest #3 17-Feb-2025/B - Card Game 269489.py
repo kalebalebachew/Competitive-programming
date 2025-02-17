159bb470c7adf9d0339721861c1f877c4a929879
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-02-17 20:31:34 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 269489.py
@@ -0,0 +1,12 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+n = int(input())
+
+cd = list(map(int, input().split()))
+
+cards = [(value, i + 1) for i, value in enumerate(cd)]
+
+cards.sort(key=lambda x: x[0])
+
+for i in range(n // 2):
+    print(cards[i][1], cards[-1 - i][1])
