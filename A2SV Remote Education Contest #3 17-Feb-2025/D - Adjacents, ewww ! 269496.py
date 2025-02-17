5a0a77380fdc6b18d3651dceb6f57923c00c0ba0
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-02-17 20:39:57 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/D - Adjacents, ewww ! 269496.py
@@ -0,0 +1,31 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    
+    if n == 2:
+        print(-1)
+        continue
+    if n == 1:
+        print(1)
+        continue
+    
+    tot = n * n
+    
+    ev = [num for num in range(1, tot + 1) if num % 2 == 0]
+    od = [num for num in range(1, tot + 1) if num % 2 == 1]
+    
+    nums = ev + od
+    
+    mat = []
+    idx = 0
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(nums[idx])
+            idx += 1
+        mat.append(row)
+    
+    for row in mat:
+        print(" ".join(map(str, row)))
