5b0cf8d10beb36435ee7b9d698296c278dce6790
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-02-17 20:32:42 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269490.py
@@ -0,0 +1,28 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+p = input()
+n = int(input())
+barks = [input() for _ in range(n)]
+
+  
+if p in barks:
+    print("YES")
+else:
+    found = False
+    
+    for fword in barks:
+        for sword in barks:
+            comb = fword + sword
+            if p in comb:
+                found = True
+                break
+        if found:
+            break
+        
+    if found:
+        print("YES")
+    else:
+        print("NO")
+
+ 
+        
