b2f917a9d6ac8327a5b31368d2937018f8271573
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-02-22 23:03:25 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 22-Feb-2025/A. Beautiful Matrix 280559.py
@@ -0,0 +1,10 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = []
+for i in range(5):
+    matrix.append(list(map(int, input().split())))
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            print(abs(i - 2) + abs(j - 2))
+            break
