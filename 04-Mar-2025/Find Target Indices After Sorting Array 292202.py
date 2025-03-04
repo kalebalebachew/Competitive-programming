db463b373d73321624264d35a8c3cb53c024f067
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-04 21:56:42 +0300

    Created 'Find Target Indices After Sorting Array' by A2SV Hub

---
+++ 04-Mar-2025/Find Target Indices After Sorting Array 292202.py
@@ -0,0 +1,17 @@
+# Problem: Find Target Indices After Sorting Array - https://leetcode.com/problems/find-target-indices-after-sorting-array/
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            midx = i
+            for j in range(i+1, len(nums)):
+                if nums[midx] > nums[j]:
+                    midx = j
+            nums[i], nums[midx] = nums[midx], nums[i]
+        for k in range(len(nums)):
+            if nums[k] == target:
+                res.append(k)
+        return res
+
+        
