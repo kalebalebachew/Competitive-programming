57f80ee25e3576365294a8081f7b3e1870934108
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-07 21:08:59 +0300

    Time: 0 ms (100%), Space: 17.8 MB (66.51%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        mx = max(arr1) 
+        count = [0] * (mx + 1)  
+        for num in arr1:
+            count[num] += 1
+        
+        res = []
+        
+        for num in arr2:
+            res.extend([num] * count[num])
+            count[num] = 0  
+        
+        for num in range(mx + 1):
+            if count[num] > 0:
+                res.extend([num] * count[num]) 
+        
+        return res
+
+
+
+
+        
