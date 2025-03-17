f7f8eefc76b33f2247aa2595f75f916e912c07fa
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-17 21:03:23 +0300

    Created 'B - The Time Heist’s Single-Shot Maneuver' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/B - The Time Heist’s Single-Shot Maneuver 309577.py
@@ -0,0 +1,28 @@
+# Problem: B - The Time Heist’s Single-Shot Maneuver - https://codeforces.com/gym/596004/problem/B
+
+t = int(input())
+res=[]
+for _ in range(t):
+  n,m=map(int,input().split())
+  a = list(map(int,input().split()))
+  b = list(map(int,input().split()))
+
+  b1=b[0]
+  possible=True
+  prev=-float('inf')
+
+  for ai in a:
+    c1=ai
+    c2=b1-ai
+    candidates=[]
+    if c1>=prev:
+      candidates.append(c1)
+    if c2>=prev:
+      candidates.append(c2)
+    if not candidates:
+      possible=False
+      break
+    prev=min(candidates)
+
+  res.append("YES" if possible else "NO")
+print("\n".join(res))
