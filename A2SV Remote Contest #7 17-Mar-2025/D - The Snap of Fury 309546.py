c29ab5d222c7411281b510c66483612b38061d31
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-17 20:26:36 +0300

    Created 'D - The Snap of Fury' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/D - The Snap of Fury 309546.py
@@ -0,0 +1,22 @@
+# Problem: D - The Snap of Fury - https://codeforces.com/gym/596004/problem/D
+
+n = int(input())
+l = list(map(int, input().split()))
+diff  = [0] * (n+1)
+
+
+for i in range(n):
+    s = max(0, i - l[i])
+    diff[s] += 1
+    diff[i] -= 1
+    
+el = 0
+sr = 0
+
+for i in range(n):
+    el += diff[i]
+    if el == 0:
+        sr += 1
+print(sr)
+
+    
