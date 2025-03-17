83a4da7e6c85e81781003fc497b863f0dd58d1ee
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-17 20:36:05 +0300

    Created 'A - Operation Infinity Assembly: The Endgame Merge' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/A - Operation Infinity Assembly: The Endgame Merge 309555.py
@@ -0,0 +1,22 @@
+# Problem: A - Operation Infinity Assembly: The Endgame Merge - https://codeforces.com/gym/596004/problem/A
+
+t = int(input())
+for _ in range(t):
+    n, m, k = map(int, input().split())
+    a = list(input().strip())
+    b = list(input().strip())
+    a.sort()
+    b.sort()
+    c = []
+    ca = 0  
+    cb = 0  
+    while a and b:
+        if (a[0] < b[0] and ca < k) or cb == k:
+            c.append(a.pop(0))
+            ca += 1
+            cb = 0  
+        else:
+            c.append(b.pop(0))
+            cb += 1
+            ca = 0  
+    print("".join(c))
