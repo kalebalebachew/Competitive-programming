bcb5818661d0c544f94eb97435617245f2dd5c7a
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-19 19:16:42 +0300

    Created 'Longest Repeating Character Replacement' by A2SV Hub

---
+++ 19-Mar-2025/Longest Repeating Character Replacement 312568.py
@@ -0,0 +1,35 @@
+# Problem: Longest Repeating Character Replacement - https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        hm = Counter()
+        res = 0
+        longest = 0
+
+        for right in range(len(s)):
+            hm[s[right]] += 1
+
+            longest = max(longest, hm[s[right]])
+
+            while (right - left + 1) - longest > k:
+                hm[s[left]] -= 1
+                left += 1
+            res = max(res, right - left + 1)
+
+        return res
+
+
+
+
+
+    
+
+
+
+     
+
+
+        
+
+        
