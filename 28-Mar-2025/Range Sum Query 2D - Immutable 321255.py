ebdca81ae16450a74570efd428d1cb398322073e
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-03-28 22:22:01 +0300

    Created 'Range Sum Query 2D - Immutable' by A2SV Hub

---
+++ 28-Mar-2025/Range Sum Query 2D - Immutable 321255.py
@@ -0,0 +1,22 @@
+# Problem: Range Sum Query 2D - Immutable - https://leetcode.com/problems/range-sum-query-2d-immutable/
+
+class NumMatrix:
+    def __init__(self, matrix):
+        if not matrix:
+            return
+        m, n = len(matrix), len(matrix[0])
+        self.prefix = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m):
+            for j in range(n):
+                self.prefix[i + 1][j + 1] = (matrix[i][j]
+                    + self.prefix[i][j + 1]
+                    + self.prefix[i + 1][j]
+                    - self.prefix[i][j])
+
+    def sumRegion(self, row1, col1, row2, col2):
+        return (
+            self.prefix[row2 + 1][col2 + 1]
+            - self.prefix[row1][col2 + 1]
+            - self.prefix[row2 + 1][col1]
+            + self.prefix[row1][col1]
+        )
