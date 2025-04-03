ec96f1a77b337f097649bc9f0fd0535436195751
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-03 20:32:05 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 03-Apr-2025/Kefa and Company 325351.py
@@ -0,0 +1,21 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+n, d = map(int, input().split())
+friends = []
+for _ in range(n):
+    m, s = map(int, input().split())
+    friends.append((m, s))
+
+friends.sort()
+mx = 0
+curr = 0
+left = 0
+
+for right in range(n):
+    curr += friends[right][1]
+    while friends[right][0] - friends[left][0] >= d:
+        curr -= friends[left][1]
+        left += 1
+    mx = max(mx, curr)
+
+print(mx)
