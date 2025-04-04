0a7bddff5a3f12f0f4e047449c398600e0318847
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-04 12:59:56 +0300

    Created 'Minimum Processing Time' by A2SV Hub

---
+++ 04-Apr-2025/Minimum Processing Time 325777.py
@@ -0,0 +1,17 @@
+# Problem: Minimum Processing Time - https://leetcode.com/problems/minimum-processing-time/
+
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        processorTime.sort()
+        tasks.sort(reverse=True) 
+        n = len(processorTime)
+        mx = 0
+        for i in range(n):
+            ps = processorTime[i]
+            for j in range(4):
+                task_index = 4 * i + j
+                cp = ps + tasks[task_index]
+                mx = max(mx, cp)
+        
+        return mx
+        
