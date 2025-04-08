a5e6973dc7d2af2d490dfa6dce82bfc9f8296626
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-08 19:17:07 +0300

    Created 'Number of Substrings Containing All Three Characters' by A2SV Hub

---
+++ 08-Apr-2025/Number of Substrings Containing All Three Characters 329101.py
@@ -0,0 +1,15 @@
+# Problem: Number of Substrings Containing All Three Characters - https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        c = {"a": 0, "b": 0, "c": 0}
+        res = 0
+        l = 0
+        for r in range(len(s)):
+            c[s[r]] += 1
+            while c['a'] > 0 and c['b'] > 0 and c['c'] > 0:
+                res += len(s) - r
+                c[s[l]] -= 1
+                l += 1
+
+        return res
