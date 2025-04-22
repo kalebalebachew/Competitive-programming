696db9a0ab81188cf4981cdb74fe8788fed36195
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-22 22:30:31 +0300

    Time: 2 ms (89.79%), Space: 18.7 MB (19.15%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -0,0 +1,43 @@
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.size = k
+        self.queue = [None] * k
+        self.front = -1
+        self.rear = -1
+        self.count = 0
+
+    def enQueue(self, value: int) -> bool:
+        if self.count == self.size:
+            return False
+        if self.count == 0:
+            self.front = 0
+        self.rear = (self.rear + 1) % self.size
+        self.queue[self.rear] = value
+        self.count += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.count == 0:
+            return False
+        self.front = (self.front + 1) % self.size
+        self.count -= 1
+        if self.count == 0:
+            self.front = -1
+            self.rear = -1
+        return True
+
+    def Front(self) -> int:
+        if self.count == 0:
+            return -1
+        return self.queue[self.front]
+
+    def Rear(self) -> int:
+        if self.count == 0:
+            return -1
+        return self.queue[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.count == 0
+
+    def isFull(self) -> bool:
+        return self.count == self.size
