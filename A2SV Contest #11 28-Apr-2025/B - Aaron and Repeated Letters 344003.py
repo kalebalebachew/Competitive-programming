7adf07a1725c6a989da2124f662f6a464f535733
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-28 12:31:46 +0300

    Created 'B - Aaron and Repeated Letters' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/B - Aaron and Repeated Letters 344003.py
@@ -0,0 +1,10 @@
+# Problem: B - Aaron and Repeated Letters - https://codeforces.com/gym/605795/problem/B
+
+n = input()
+stack = []
+for char in n:
+    if stack and stack[-1] == char:
+        stack.pop()
+    else:
+        stack.append(char)
+print(''.join(stack))
