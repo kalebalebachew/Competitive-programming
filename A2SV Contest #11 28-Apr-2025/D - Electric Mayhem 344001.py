a039305fa7938bd655dc1ca8a608be24f8880264
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-28 12:31:27 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344001.py
@@ -0,0 +1,16 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+s = input().strip()
+n = len(s)
+if n & 1:
+    print("No")
+    exit()
+
+stack = []
+for c in s:
+    if stack and stack[-1] == c:
+        stack.pop()
+    else:
+        stack.append(c)
+
+print("Yes" if not stack else "No")
