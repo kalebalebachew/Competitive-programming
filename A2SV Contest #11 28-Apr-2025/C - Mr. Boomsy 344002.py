cc09cf4f6e695ae3ffe724641a7c07bcfed139ce
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-28 12:31:33 +0300

    Created 'C - Mr. Boomsy' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/C - Mr. Boomsy 344002.py
@@ -0,0 +1,12 @@
+# Problem: C - Mr. Boomsy - https://codeforces.com/gym/605795/problem/C
+
+t = int(input())
+for _ in range(t):
+        s = input().strip()
+        stack = []
+        for ch in s:
+            if ch == 'B' and stack and stack[-1] in ('A', 'B'):
+                stack.pop()
+            else:
+                stack.append(ch)
+        print(len(stack))
