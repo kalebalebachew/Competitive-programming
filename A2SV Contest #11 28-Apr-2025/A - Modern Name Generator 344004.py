9ebb633b0f63643489f13ee2aa11a13493570a14
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-04-28 12:31:53 +0300

    Created 'A - Modern Name Generator' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/A - Modern Name Generator 344004.py
@@ -0,0 +1,8 @@
+# Problem: A - Modern Name Generator - https://codeforces.com/gym/605795/problem/A
+
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    a, b, c = input().split()
+    print(a[0] + b[0] + c[0])
