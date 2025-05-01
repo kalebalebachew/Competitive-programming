94f83c119fc4b21b44ca062e0baa6a359cb7de9b
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-05-01 21:39:57 +0300

    Created 'Super Pow' by A2SV Hub

---
+++ 01-May-2025/Super Pow 346576.py
@@ -0,0 +1,8 @@
+# Problem: Super Pow - https://leetcode.com/problems/super-pow/description/
+
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        res = 1
+        for d in b:
+            res = ((res ** 10) * (a**d % 1337)) % 1337
+        return res
