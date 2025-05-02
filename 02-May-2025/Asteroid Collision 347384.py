9af0f7978c38e0012cbbf3ffbf9671ba71651c92
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-05-02 22:56:11 +0300

    Created 'Asteroid Collision' by A2SV Hub

---
+++ 02-May-2025/Asteroid Collision 347384.py
@@ -0,0 +1,26 @@
+# Problem: Asteroid Collision - https://leetcode.com/problems/asteroid-collision/
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            while True:
+                if not stack or asteroid > 0 or stack[-1] < 0 or (stack[-1] > 0 and asteroid > 0):
+                    stack.append(asteroid)
+                    break
+                elif stack[-1] > 0 and asteroid < 0:
+                    if stack[-1] < abs(asteroid):
+                        stack.pop()
+                        if not stack:
+                            stack.append(asteroid)
+                            break
+                        continue
+                    elif stack[-1] == abs(asteroid):
+                        stack.pop()
+                        break
+                    else:
+                        break
+                else:
+                    break
+        
+        return stack
