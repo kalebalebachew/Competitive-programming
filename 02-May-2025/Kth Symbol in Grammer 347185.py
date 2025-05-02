2db31a0cc1d7582ca4025efdcd7706fbda586ab3
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-05-02 21:15:32 +0300

    Created 'Kth Symbol in Grammer' by A2SV Hub

---
+++ 02-May-2025/Kth Symbol in Grammer 347185.py
@@ -0,0 +1,11 @@
+# Problem: Kth Symbol in Grammer - https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        p = 1 << (n - 2) 
+        if k <= p:
+            return self.kthGrammar(n - 1, k)
+        else:
+            return 1 - self.kthGrammar(n - 1, k - p)
