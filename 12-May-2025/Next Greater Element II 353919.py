9c06a5deaaabfe79e01c4350e1b12c7e5f767c6a
Kaleb <118438724+kalebalebachew@users.noreply.github.com>
2025-05-12 21:29:42 +0300

    Created 'Next Greater Element II' by A2SV Hub

---
+++ 12-May-2025/Next Greater Element II 353919.py
@@ -0,0 +1,17 @@
+# Problem: Next Greater Element II - https://leetcode.com/problems/next-greater-element-ii/
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        res = [-1] * len(nums)
+        s = []
+        for i in range(len(nums)):
+            while s and nums[s[-1]] < nums[i]:
+                res[s[-1]] = nums[i]
+                s.pop()
+            s.append(i)
+        for i in range(len(nums)):
+            while s and nums[s[-1]] < nums[i]:
+                res[s[-1]] = nums[i]
+                s.pop()
+        return res
+        
